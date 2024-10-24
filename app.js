fb7f5713a9146512503ef5780a018135205c808a
praveendev0327 <praveensubramaniyandev@gmail.com>
2024-10-24 12:08:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 const db = require('./config/db');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const nodemailer = require('nodemailer');
 
 // Import Routes
 const authRoutes = require('./routes/auth');
@@ -21,6 +22,61 @@ const selectclients = [];
 
 
 app.use(express.json());
+
+// Configure Nodemailer transport
+const transporter = nodemailer.createTransport({
+    service: 'gmail', // Use 'gmail' or other email service
+    auth: {
+      user: 'praveensubramaniyandev@gmail.com', // Your email
+      pass: 'brlu rflo phjn sllt', // Your email password (use App Passwords for better security)
+    },
+  });
+
+  // API endpoint to send an email
+app.post('/send-email', (req, res) => {
+    const { to, subject, name } = req.body;
+
+
+    const emailBody = `
+Dear ${name},
+
+Thank you for registering for our exciting Lucky Draw! We are thrilled to have you on board. Your participation brings us one step closer to an exhilarating event.
+
+Here are the details you need to know:
+
+- **Event Date:** November 3
+- **Time:** 6 PM
+- **Venue:** Al Nasr Leisureland, Oud Metha Dubai.
+- **Prize:** Exciting prizes await, including gold coins, gift vouchers, and more!
+
+Stay tuned for more updates as we approach the event date. Don’t miss your chance to win amazing prizes!
+
+If you have any questions, feel free to reach out.
+
+Best of luck!
+
+Warm regards,
+
+Muthamil Sangam 
+`;
+  
+    const mailOptions = {
+      from: 'praveensubramaniyandev@gmail.com',
+      to,
+      subject,
+      text: emailBody,
+    };
+  
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log("error",error);
+        return res.status(500).send(error.toString());
+      }
+    //   console.log('Email sent: ' + info.response);
+      res.status(200).send('Email sent: ' + info.response);
+    });
+  });
+  
 
 app.post('/upload', (req, res) => {
    
@@ -100,8 +156,10 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: 'Server Error', error: err.message });
 });
 
-// Start the Server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+// Start the Server process.env.PORT || 5
+const PORT = 3000;
+app.listen(PORT,'0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+
